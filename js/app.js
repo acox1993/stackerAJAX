f5e0cc4066753e8de0646a88e85c31cb6c50b612
acox1993 <alexandriacrmu@gmail.com>
2016-06-26 09:25:51 -0400

    recent
    issue with length

--- js/app.js
+++ js/app.js
@@ -81,6 +81,39 @@ var getUnanswered = function(tags) {
 	});
 };
 
+var getAnswerers = function(answerer) {
+	console.log('test');
+	// the parameters we need to pass in our request to StackOverflow's API
+	var request = { 
+		tagged: answerer,
+		site: 'stackoverflow',
+		order: 'desc',
+		sort: 'creation'
+	};
+	
+	$.ajax({
+		url: "http://api.stackexchange.com/2.2/tags/"+ request.tagged +"/top-answerers",
+		data: request,
+		dataType: "jsonp",//use jsonp to avoid cross origin issues
+		type: "GET",
+	})
+	.done(function(result){ //this waits for the ajax to return with a succesful promise object
+		// var searchResults = showSearchResults(request.tagged, result.items.length);
+		var answererResults = showSearchResults(request.tag);
+
+		$('.search-results').html(answererResults);
+		//$.each is a higher order function. It takes an array and a function as an argument.
+		//The function is executed once for each item in the array.
+		$.each(result.items, function(i, item) {
+			var question = showQuestion(item);
+			$('.results').append(question);
+		});
+	})
+	.fail(function(jqXHR, error){ //this waits for the ajax to return with an error promise object
+		var errorElem = showError(error);
+		$('.search-results').append(errorElem);
+	});
+};
 
 $(document).ready( function() {
 	$('.unanswered-getter').submit( function(e){
@@ -90,5 +123,11 @@ $(document).ready( function() {
 		// get the value of the tags the user submitted
 		var tags = $(this).find("input[name='tags']").val();
 		getUnanswered(tags);
+	});
+	$('.inspiration-getter').submit( function(e){
+		e.preventDefault();
+		$('.results').html('');
+		var  answerer = $(this).find("input[name='answerer']").val();
+		getAnswerers(answerer);
 	});
 });
